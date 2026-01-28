546eaf85e88a2661283e805b1e2af930a00d659f
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2026-01-28 14:43:25 +0000

    initial project files

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Thank you for using our free map, it means a lot to use and motivates us to create more as we go along our journey in map development :)
+
+fx_version 'cerulean'
+game 'gta5'
+author 'One Mapping'
+decsription 'Sleek legion square torture room with lighting'
+
+files {
+    'stream/ydrs/*.ydr',
+    'stream/ybns/*.ybn',
+    'stream/ymaps/*.ymap',
+    'stream/*.ymf'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ydrs/*.ydr'
