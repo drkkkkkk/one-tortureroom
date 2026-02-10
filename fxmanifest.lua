f5f8ba30e6445a6a37db7bff06e5917745ad0c50
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2026-02-10 08:46:22 +0000

    Fix spelling error in fxmanifest description
    Corrected the spelling of 'description' in the fxmanifest.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,7 @@
--- Thank you for using our free map, it means a lot to use and motivates us to create more as we go along our journey in map development :)
-
 fx_version 'cerulean'
 game 'gta5'
+
 author 'One Mapping'
-decsription 'Sleek legion square torture room with lighting'
+description 'Sleek Legion Square torture room with lighting'
 
-files {
-    'stream/ydrs/*.ydr',
-    'stream/ybns/*.ybn',
-    'stream/ymaps/*.ymap',
-    'stream/*.ymf'
-}
-
-data_file 'DLC_ITYP_REQUEST' 'stream/ydrs/*.ydr'
+this_is_a_map 'yes'
